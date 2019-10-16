1a970b961a8e7cb295afc12fa8d40fc498de620c
silasfan <fettergr@163.com>
2019-10-16 12:33:06 +0800

    fix: constantly request

---
+++ precache-manifest.5852d2c5e635046bf949909c48b35f91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1afe5faf37818e81649",
+    "url": "/NTT-Insights/css/app.256c9155.css"
+  },
+  {
+    "revision": "b1afe5faf37818e81649",
+    "url": "/NTT-Insights/js/app-legacy.57a3e069.js"
+  },
+  {
+    "revision": "fc806c9c764e137d2be3",
+    "url": "/NTT-Insights/css/chunk-vendors.755a9159.css"
+  },
+  {
+    "revision": "fc806c9c764e137d2be3",
+    "url": "/NTT-Insights/js/chunk-vendors-legacy.313492f7.js"
+  },
+  {
+    "revision": "72411f3e089658781c65325eca5e7c0e",
+    "url": "/NTT-Insights/index.html"
+  }
+];
---
+++ precache-manifest.e5f2d98a971ae1299271e7a5267b095a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b175b42b09e9b969a06e",
+    "url": "/NTT-Insights/css/app.256c9155.css"
+  },
+  {
+    "revision": "b175b42b09e9b969a06e",
+    "url": "/NTT-Insights/js/app.ccf6d2d8.js"
+  },
+  {
+    "revision": "5d4549a68685b5e14749",
+    "url": "/NTT-Insights/css/chunk-vendors.755a9159.css"
+  },
+  {
+    "revision": "5d4549a68685b5e14749",
+    "url": "/NTT-Insights/js/chunk-vendors.1d747004.js"
+  },
+  {
+    "revision": "0394e96e9b730c383a504901e81d6959",
+    "url": "/NTT-Insights/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/NTT-Insights/robots.txt"
+  }
+];
